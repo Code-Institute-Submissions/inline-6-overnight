8dd8a91b189725f8819b87c5a80a7ed867bb60fc
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-11-05 11:46:40 +0000

    Create the views for the store.html, cart.html and checkout.html templates

--- store/views.py
+++ store/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def store(request):
+    """
+    Django function-based view for the store (or home) page
+    """
+    context = {}
+    return render(request, 'store/store.html', context)
+
+
+def cart(request):
+    """
+    Django function-based view for the cart page
+    """
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    """
+    Django function-based view for the checkout page
+    """
+    context = {}
+    return render(request, 'store/checkout.html', context)
