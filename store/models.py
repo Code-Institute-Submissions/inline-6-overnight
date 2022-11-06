6cb6046d1d63f23e9d3c5d3b30a629c4b0dd12fe
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-11-06 13:19:38 +0000

    Create the Product, Order and OrderItem models

--- store/models.py
+++ store/models.py
@@ -3,10 +3,52 @@ from django.contrib.auth.models import User
 
 
 class Customer(models.Model):
+    """
+    Customer model
+    """
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
-    email = models.EmailField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    """
+    Product model
+    """
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
+    # image
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    """
+    Order model
+    """
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    """
+    OrderItem model
+    """
+    product = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(
+        Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
