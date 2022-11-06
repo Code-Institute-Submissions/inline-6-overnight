fb3522426581b0738940d00f0a2b296a95287f76
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-11-06 18:46:59 +0000

    Create a customer and an order in the admin area and query the data for both in views.py

--- store/views.py
+++ store/views.py
@@ -15,7 +15,15 @@ def cart(request):
     """
     Django function-based view for the cart page
     """
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # Create empty cart for now for non-logged in users
+        items = []
+    context = {'items': items}
     return render(request, 'store/cart.html', context)
 
 
