71537c1b1805125fda0466b3fc70983487cf8a0e
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-11-07 18:42:45 +0000

    Create the view and the url path to send the add to cart button data to the view if the user is logged in

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
 
 
@@ -45,3 +46,11 @@ def checkout(request):
 
     context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    """
+    Django function-based view to create/add quantity to an OrderItem
+    or remove/delete the item if the action sent over is 'remove'
+    """
+    return JsonResponse('Item was added', safe=False)
