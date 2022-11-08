934a181e938e3ef0c6935614bd144502bf43a49e
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-11-08 18:32:48 +0000

    Create the logic to define the order's shipping status by adding the shipping attribute to views.py

--- store/views.py
+++ store/views.py
@@ -10,13 +10,14 @@ def store(request):
     """
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
         # Create an empty cart for now for non-logged in user
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
         cartItems = order['get_cart_items']
     products = Product.objects.all()
     context = {'products': products, 'cartItems': cartItems}
@@ -36,7 +37,7 @@ def cart(request):
     else:
         # Create empty cart for now for non-logged in users
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
         cartItems = order['get_cart_items']
 
     context = {'items': items, 'order': order, 'cartItems': cartItems}
@@ -56,7 +57,7 @@ def checkout(request):
     else:
         # Create empty cart for now for non-logged in users
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
         cartItems = order['get_cart_items']
 
     context = {'items': items, 'order': order, 'cartItems': cartItems}
