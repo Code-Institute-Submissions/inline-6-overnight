1f3c2abeb42b37d3348bfdcdc2dd5fd29783c3bf
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-11-20 18:04:28 +0000

    Fix line too long in manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inline_6_overnight.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE', 'inline_6_overnight.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
