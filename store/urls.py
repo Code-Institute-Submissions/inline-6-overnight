2eb1a7a4e4499fb1dc527fe1c1ffea09f28a098e
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-11-20 10:53:46 +0000

    Add functional linkfto the admin area for superusers only to the navbar

--- store/urls.py
+++ store/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('update_item/', views.updateItem, name="update_item"),
     path('process_order/', views.processOrder, name="process_order"),
     path('signup/', views.signup, name='signup'),
+    path('admin/', views.logout_view, name='admin'),
     path('logout/', views.logout_view, name='logout'),
 ]
