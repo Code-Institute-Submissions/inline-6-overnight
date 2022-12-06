457c57beb4592c3f8cbd20a04a180f469d5d86d7
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-12-06 11:43:53 +0000

    Remove function-based view from the 404 error page

--- inline_6_overnight/views.py
+++ inline_6_overnight/views.py
@@ -2,21 +2,5 @@ from django.shortcuts import render
 
 
 def handler404(request, exception):
-    """
-    Django function-based view for the 404 error page
-    """
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(
-            customer=customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        # Create empty cart for now for non-logged in users
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
-        cartItems = order['get_cart_items']
-
-    context = {'items': items, 'order': order, 'cartItems': cartItems}
     """ Error Handler 404 - Page Not Found """
-    return render(request, "errors/404.html", context, status=404)
+    return render(request, "errors/404.html", status=404)
