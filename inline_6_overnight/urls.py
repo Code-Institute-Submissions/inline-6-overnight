4bf1e992174c0632efffd413f89640d29bbc47d4
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-12-06 10:36:49 +0000

    Create the 404 error page

--- inline_6_overnight/urls.py
+++ inline_6_overnight/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +25,5 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'inline_6_overnight.views.handler404'
