b3e2aa2bce53c518ef8eae9824bf4d0f4fc15b1a
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-12-06 12:29:28 +0000

    Resolve issue with cart not showing on 404 page

--- inline_6_overnight/views.py
+++ inline_6_overnight/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from store.models import Order
 
 
 def handler404(request, exception):
+    """
+    Django function-based view for the 404 error page
+    """
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        # Create empty cart for now for non-logged in users
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
+        cartItems = order['get_cart_items']
+
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
+
     """ Error Handler 404 - Page Not Found """
-    return render(request, "errors/404.html", status=404)
+    response = render(request, "errors/404.html", context=context)
+    response.status_code = 404
+    return response
