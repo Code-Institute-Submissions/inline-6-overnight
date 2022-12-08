c3c163125c4a907a561559e470ce013a52d76b7f
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-12-08 16:18:13 +0000

    Fix some code validation errors in cart.js

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,25 +1,25 @@
-var updateBtns = document.getElementsByClassName('update-cart')
+var updateBtns = document.getElementsByClassName('update-cart');
 
 for (var i = 0; i < updateBtns.length; i++) {
 	updateBtns[i].addEventListener('click', function(){
-		var productId = this.dataset.product
-		var action = this.dataset.action
-		console.log('productId:', productId, 'Action:', action)
+		var productId = this.dataset.product;
+		var action = this.dataset.action;
+		console.log('productId:', productId, 'Action:', action);
 
-		console.log('USER:', user)
+		console.log('USER:', user);
 		if (user === 'AnonymousUser') {
-			console.log('Not logged in')
+			console.log('Not logged in');
 					
 		} else {
-			updateUserOrder(productId, action)
+			updateUserOrder(productId, action);
 		}
-	})
+	});
 }
 
 function updateUserOrder(productId, action) {
-	console.log('User is logged in, sending data...')
+	console.log('User is logged in, sending data...');
 
-		var url = '/update_item/'
+		var url = '/update_item/';
 
 		fetch(url, {
 			method:'POST',
@@ -30,10 +30,10 @@ function updateUserOrder(productId, action) {
 			body:JSON.stringify({'productId': productId, 'action': action})
 		})
 		.then((response) => {
-		   return response.json()
+		   return response.json();
 		})
 		.then((data) => {
-		    console.log('data:', data)
-			location.reload()
-		})
+		    console.log('data:', data);
+			location.reload();
+		});
 }
